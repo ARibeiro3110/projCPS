25e137b080ea51395287e19ea999972937366cf1
lauraaa8 <lauraafonsoguerreiro@gmail.com>
2026-05-31 16:54:50 +0100

    Cenas cenas

--- GSWParams.py
+++ GSWParams.py
@@ -1,8 +1,8 @@
-from numpy import ceil, log2
+from numpy import ceil, log2, sqrt
 
 class GSWParams:
 
-    def __init__(self, L:int, n: int, q: int, hardness: str = "standard", k: int = 5):
+    def __init__(self, L:int, n: int, q: int, hardness: str = "standard", k: int = 3):
         self.L = L
         self.n = n
         self.q = q
@@ -10,7 +10,7 @@ class GSWParams:
         self.N = self.ell * (self.n + 1)
         self.m = 2*self.ell * self.n #After lemma 1 it states that it suffices m>2nlog2(q) for (A,R.A) to be undist. from uniform
         if hardness == "standard":
-            self.sigma = q/(k*(8*self.m*((self.N+1)**self.L))) # TODO: Confirmar valor de k e add toy parameters and real parameters
+            self.sigma = q/(k*(8*sqrt(self.m)*((self.N+1)**self.L))) # TODO: Confirmar valor de k e add toy parameters and real parameters
         else:
             self.sigma =  3.2 #TODO: Confirmar valor de sigma para toy parameters
 
--- testes.py
+++ testes.py
@@ -6,13 +6,13 @@ from GSWScheme import *
 
 
 scheme = GSWScheme()
-scheme.Setup(L=0, n=8, q=2**15, hardness="standard")
+scheme.Setup(L=3, n=4, q=2**30, hardness="standard")
 print("Testing GSW Scheme for parameters\n", scheme.params)
 
 sk = scheme.SecretKeyGen()
 scheme.PublicKeyGen(sk)
 pk=scheme.getPublicKey()
-print("Secret Key:\n", sk.getSecretKey())
+#print("Secret Key:\n", sk.getSecretKey())
 #print("Public Key:\n", pk.getPublicKey())
 #print("error e:\n", pk.getPublicKey()@sk.getSecretKey()%scheme.params.get_q())
 
@@ -20,13 +20,13 @@ mu1 = 0
 mu2 = 1
 C1 = scheme.Enc(pk, mu=mu1)
 print("Decrypted 1:\n", scheme.Dec(sk, C1))
-print("Abs error 1:\n", scheme.DecTest(sk, C1, mu=0))
+print("Abs error 1:\n", scheme.DecTest(sk, C1, mu=mu1))
 C2 = scheme.Enc(pk, mu=mu2)
 print("Decrypted 2:\n", scheme.Dec(sk, C2))
 print("Abs error 2:\n", scheme.DecTest(sk, C2, mu=mu2))
 #C3 = scheme.NAND(C1, C2)
 for i in range(5):
-    print("Iteration:\n", i+1)
+    print("#### Iteration:", f"{i+1}", "####")
     C = scheme.NAND(C1, C2)
     mu = (0 if mu1 == 1 and mu2 == 1 else 1)
     print("mu:", mu)
@@ -39,7 +39,3 @@ for i in range(5):
     C2=C
     mu2=mu
 
-
-#print("NAND Result:\n", scheme.Dec(sk, C3))
-C4 = scheme.Enc(pk, mu=14)
-print("Decrypted 4:\n", scheme.MPDec(sk, C4))
