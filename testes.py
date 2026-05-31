9a6eaa416665d4d477459ce4f33f4c886654d6d3
lauraaa8 <lauraafonsoguerreiro@gmail.com>
2026-05-31 17:03:51 +0100

    Testes circuitos

--- testes.py
+++ testes.py
@@ -7,7 +7,7 @@ from GSWScheme import *
 
 scheme = GSWScheme()
 scheme.Setup(L=3, n=4, q=2**30, hardness="standard")
-print("Testing GSW Scheme for parameters\n", scheme.params)
+print("\nTesting GSW Scheme for parameters\n", scheme.params, "\n")
 
 sk = scheme.SecretKeyGen()
 scheme.PublicKeyGen(sk)
@@ -19,21 +19,24 @@ pk=scheme.getPublicKey()
 mu1 = 0
 mu2 = 1
 C1 = scheme.Enc(pk, mu=mu1)
-print("Decrypted 1:\n", scheme.Dec(sk, C1))
-print("Abs error 1:\n", scheme.DecTest(sk, C1, mu=mu1))
+print("mu1:", mu1)
+print("Decrypted 1:", scheme.Dec(sk, C1))
+print("Error Decryption 1:", scheme.DecTest(sk, C1, mu=mu1))
 C2 = scheme.Enc(pk, mu=mu2)
-print("Decrypted 2:\n", scheme.Dec(sk, C2))
-print("Abs error 2:\n", scheme.DecTest(sk, C2, mu=mu2))
+print("\nmu2:", mu2)
+print("Decrypted 2:", scheme.Dec(sk, C2))
+print("Error Decryption 2:", scheme.DecTest(sk, C2, mu=mu2))
 #C3 = scheme.NAND(C1, C2)
 for i in range(5):
-    print("#### Iteration:", f"{i+1}", "####")
+
+    print("\n##### Iteration:", f"{i+1}", "#####")
     C = scheme.NAND(C1, C2)
     mu = (0 if mu1 == 1 and mu2 == 1 else 1)
     print("mu:", mu)
-    print("Decrypted NAND:\n", scheme.Dec(sk, C))
+    print("Decrypted NAND:", scheme.Dec(sk, C))
     e=scheme.DecTest(sk, C, mu=mu)
-    print("Abs error NAND:\n", e)
-    print(e<scheme.params.get_q()/8)
+    print("Error Decryption NAND:", e)
+    print("Error < q/8:", e<scheme.params.get_q()/8)
     C1=C2
     mu1=mu2
     C2=C
